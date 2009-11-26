76fd7ef2378fbd7212097095c8cc2073bbc0ed29
Sheldon Dueck <sheldon.dueck@gmail.com>
2009-11-25 21:01:52 -0500

    Add seeds for models

---
+++ db/fixtures/7_registration_statuses.rb
@@ -0,0 +1,17 @@
+RegistrationStatus.seed_many([
+
+  {
+    :id => 1,
+    :description => "Registered"
+  },
+  {
+    :id => 2,
+    :description => "Cancelled"
+  },
+  {
+    :id => 3,
+    :description => "Incomplete"
+  }
+
+])
+
