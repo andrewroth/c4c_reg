817bf57c7a99f1dd80a85b8123869fcf0ee79505
Sheldon Dueck <sheldon.dueck@gmail.com>
2009-11-30 10:37:37 -0500

    Create helper method for event start date

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,28 @@
 module EventsHelper
+
+  def format_event_start_date(event)
+
+    date = ""
+
+    if event.start != nil && event.end != nil then
+
+      start_date = Date.parse(event.start.to_s)
+      end_date = Date.parse(event.end.to_s)
+
+      if end_date < Date.today then
+        date = "Over"
+      elsif start_date < Date.today then
+        date = "Ongoing!"
+      else
+        date = start_date.strftime("%B %e, %Y")
+      end
+
+    else
+      date = "Unknown"
+    end
+
+    date
+  end
+
+
 end
