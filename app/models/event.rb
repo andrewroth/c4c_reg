f77497e7ca7fec654b68dd2f63924bb398e54024
Sheldon Dueck <sheldon.dueck@gmail.com>
2009-12-14 21:52:21 -0500

    Make event model neater

--- app/models/event.rb
+++ app/models/event.rb
@@ -50,16 +50,6 @@ class Event < ActiveRecord::Base
 
     campus_info = ActiveSupport::OrderedHash.new # will contain gender and registration status info for each campus
     
-#    registrations = ActiveSupport::OrderedHash.new # will contain a reference to each registration with it's status and that person's gender
-    
-
-#    campuses = Campus.all( :include => { :assignments => { :person => [ { :registrations => :registration_status_assoc }, :gender ] } },
-#
-#                           :select => "#{__(:description, :campus)}, #{__(:description, :gender)}, #{__(:description, :registration_status)}",
-#
-#                           :conditions => [ "#{__(:event_id, :registration)} = ?", self.id ] )
-
-
     campuses = Assignment.all( :joins => [ :campus, { :person => [ { :registrations => :registration_status_assoc }, :gender ] } ],
 
                                :select => "#{__(:id, :campus)} AS campusID, " +
@@ -89,89 +79,6 @@ class Event < ActiveRecord::Base
 
     # sort the hash by campus name
     campus_info = campus_info.sorted_hash{ |a, b| a[1][:name] <=> b[1][:name]}
-    
-    
-
-
-
-
-
-
-
-#
-#    campuses = Assignment.all( :joins => [ :person => [ { :registrations => :registration_status_assoc }, :gender ] ],
-#
-#                               :select => "DISTINCT #{__(:id, :person)}, " +
-#                                          "SUM( #{__(:description, :gender)} = '#{Gender::MALE}') AS males, " +
-#                                          "SUM( #{__(:description, :gender)} = '#{Gender::FEMALE}') AS females, " +
-#                                          "SUM( #{__(:description, :gender)} = '#{Gender::UNKNOWN}') AS unknowns, " +
-#                                          "SUM( #{__(:description, :registration_status)} = '#{RegistrationStatus::CANCELLED}') AS cancelled, " +
-#                                          "SUM( #{__(:description, :registration_status)} = '#{RegistrationStatus::REGISTERED}') AS registered, " +
-#                                          "SUM( #{__(:description, :registration_status)} = '#{RegistrationStatus::INCOMPLETE}') AS incomplete",
-#
-#                               :conditions => [ "#{__(:event_id, :registration)} = ?", self.id ] )
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#    assignments = Assignment.all( :joins => [ { :person => [ :gender, {:registrations => :registration_status_assoc} ] }, :campus ],
-#
-#                                  :select => "#{__(:id, :campus)} AS campus_id, " +
-#                                             "#{__(:description, :campus)} AS campus_description, " +
-#                                             "#{__(:description, :gender)} AS gender, " +
-#                                             "#{__(:description, :registration_status)} AS registration_status, " +
-#                                             "#{__(:id, :registration)} AS registration_id",
-#
-#                                  :conditions => [ "#{Registration.table_name}.#{_(:event_id, :registration)} = ?", self.id ] )
-#
-#
-#    # go through each person's (who has registered for this event) assignments
-#    assignments.each do |assignment|
-#
-#      # if we haven't encountered this campus_id yet add it to the campus_info hash
-#      merge_if_not_present(campus_info, assignment.campus_id.to_i => { :name => assignment.campus_description,
-#                                                                       :gender => {},
-#                                                                       :status => {} })
-#
-#      # if we haven't encountered this gender yet add it to the campus_id hash
-#      merge_if_not_present( campus_info[assignment.campus_id.to_i][:gender], assignment.gender => 0 )
-#
-#      # if we haven't encountered this registration_status yet add it to the campus_id hash
-#      merge_if_not_present( campus_info[assignment.campus_id.to_i][:status], assignment.registration_status => 0 )
-#
-#      campus_info[assignment.campus_id.to_i][:gender][assignment.gender] += 1
-#      campus_info[assignment.campus_id.to_i][:status][assignment.registration_status] += 1
-#
-#      registrations.merge!( { assignment.registration_id => { :gender => assignment.gender, :status => assignment.registration_status }} )
-#    end
-#
-#    # sort the hash by campus name
-#    campus_info = campus_info.sorted_hash{ |a, b| a[1][:name] <=> b[1][:name]}
-#
-#
-#    # find totals for gender and registration status
-#
-#    campus_info.merge!( :total => { :name => "Totals", :gender => {}, :status => {} } )
-#
-#    registrations.each_value do |info|
-#
-#      # if we haven't encountered this gender_id yet add it to the campus_id hash
-#      merge_if_not_present( campus_info[:total][:gender], info[:gender] => 0 )
-#
-#      # if we haven't encountered this registration_status yet add it to the campus_id hash
-#      merge_if_not_present( campus_info[:total][:status], info[:status] => 0 )
-#
-#      campus_info[:total][:gender][info[:gender]] += 1
-#      campus_info[:total][:status][info[:status]] += 1
-#
-#    end
 
     campus_info
   end
@@ -186,22 +93,6 @@ class Event < ActiveRecord::Base
                                  "#{__(:description, :registration_status)}, #{__(:date, :registration)}, #{__(:balance, :registration)}, #{__(:id, :registration)}",
 
                       :conditions => [ "#{__(:event_id, :registration)} = ? AND #{__(:id, :campus)} = ?", self.id, campus.id ] )
-
-
-#    Registration.all( :include => [ { :person => { :assignments => :campus } }, :registration_status_assoc, :cash_transactions ],
-
-#                                      :select => "#{Campus.table_name}.#{_(:id, :campus)} AS campus_id, " +
-#                                                 "#{Person.table_name}.#{_(:first_name, :person)} AS first_name, " +
-#                                                 "#{Person.table_name}.#{_(:last_name, :person)} AS last_name, " +
-#                                                 "#{Person.table_name}.#{_(:email, :person)} AS email, " +
-#                                                 "#{RegistrationStatus.table_name}.#{_(:description, :registration_status)} AS registration_status, " +
-#                                                 "#{Registration.table_name}.#{_(:date, :registration)} AS date, " +
-#                                                 "#{Registration.table_name}.#{_(:balance, :registration)} AS balance, " +
-#                                                 "#{Registration.table_name}.#{_(:id, :registration)} AS registration_id",
-
-#                                      :conditions => [ "#{Registration.table_name}.#{_(:event_id, :registration)} = ? AND " +
-#                                                       "#{Campus.table_name}.#{_(:id, :campus)} = ?", +
-#                                                       self.id, campus.id ] )
   end
 
 
